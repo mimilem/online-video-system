e248dfa16436cfb907daf5cd8dcf3cf83a924894
scott tresor <scotttresor@gmail.com>
2022-04-11 17:53:33 +0200

    [FIX] fix link with to join room

--- resources/views/mails/index.blade.php
+++ resources/views/mails/index.blade.php
@@ -1,5 +1,8 @@
 <h1>Hi</h1>
 <p>You have created virtual meeting. The following information can be used to join room </p>
-<p>nRoomID : ".{{ $rooms['room']['room_id'] }}</p>
+<p>RoomID : ".{{ $rooms['room']['room_id'] }}</p>
 <p>Facilitator Room Pin : ".{{ $pinsCode }}</p>
-<p>Student Room Pin : ".{{ $pinsCode }}</p>
+<p>Student Room Pin : ".{{ $participant }}</p>
+<a href="{{ route('room.getRoom', ['room' => $rooms['room']['room_id']] }}">Join Event</a>
+
+<p>Thank you to user Aperi</p>
